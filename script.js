7bc73ff4e6b006afdae406f2ef6abe8f4d0444f7
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2022-01-28 20:03:23 +0300

    added hard homework to lesson02

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let num = 266219;
+let str = String(num);
+console.log(typeof str);
+let strToArr = Array.from(str);
+console.log(strToArr);
+let strArrToNum = strToArr.map(Number);
+console.log(strArrToNum);
+let count = 1;
 
+strArrToNum.forEach(function(entry) {
+  count *= entry;
+  
+});
+
+console.log(count);
+
+count **= 3;
+console.log(count);
+
+let countToStr = String(count);
+let res = Number(countToStr.substr(0,2));
+console.log(res);
