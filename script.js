915852da8acb19145625608558f6e6b53df0391a
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2022-01-31 14:41:48 +0300

    added hard homework to lesson03

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ console.log(countNum);
 let res = Number(String(countNum).substr(0,2));
 console.log(res);
 
+
+let lang = "en";
+
+if( lang == "ru") {
+  console.log("Понедельник");
+  console.log("Вторник");
+  console.log("Среда");
+  console.log("Четверг");
+} else if ( lang == "en") {
+  console.log("Monday");
+  console.log("Thuesday");
+  console.log("Wednesday");
+  console.log("Sunday");
+}
+
+switch (lang) {
+  case "ru":
+    console.log("Понедельник");
+    console.log("Вторник");
+    console.log("Среда");
+    console.log("Четверг");
+    break
+  case "en":
+    console.log("Monday");
+    console.log("Thuesday");
+    console.log("Wednesday");
+    console.log("Sunday");
+    break
+}
+
+const langArray = [ 
+  
+   ["Monday", "Thuesday", "Wednesday", "Sunday"], 
+
+   ["Понедельник",  "Вторник", "Среда", "Четверг"]
+
+];
+
+langArray[0].forEach(function (entry) {
+  console.log(entry);
+});
+
+
+let namePerson = "Александр";
+
+namePerson == "Артем" ? console.log("директор") : console.log("студент");
+namePerson == "Александр" ? console.log("преподаватель") : console.log("студент");
